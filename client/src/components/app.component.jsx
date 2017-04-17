e5caec309ec21a85632fd0316f4315dc433b0af7
Daniel <daniel.seehausen@gmail.com>
2017-04-17 16:31:08 -0400

    minor change

--- client/src/components/app.component.jsx
+++ client/src/components/app.component.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { render } from 'react-dom'
+import { render } from 'react-dom'
 
 export default class App extends Component {
   constructor() {
--- client/src/stores/store.js
+++ client/src/stores/store.js
@@ -6,3 +6,13 @@ export function configureStore(){
 }
 
 export const store = configureStore()
+import { createStore } from 'redux'
+import rootReducer from './reducers/index'
+
+export function configureStore(){
+  return createStore(rootReducer)
+}
+
+export const store = configureStore()
+
+
