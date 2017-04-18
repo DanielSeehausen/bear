abd93ef4ebbd1c47f60d861b35a469f095d18aae
github username <github email address>
2017-04-18 17:30:26 -0400

    Updated routes

--- server/bear_api/config/routes.rb
+++ server/bear_api/config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   delete '/logout', to: "sessions#destroy"
 
   resources :user_games
-  resources :game_rounds, only: [:new, :index, :show]
+  resources :game_rounds, only: [:new, :index, :create, :show]
   resources :users, only: [:new, :index, :show, :create]
   resources :sessions, only: [:new, :create, :destroy]
 
