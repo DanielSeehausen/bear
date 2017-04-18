4365a1b8c1f650355dc6907b716a8f142a43787c
NickAnaya618 <anaya.nicholas@gmail.com>
2017-04-18 11:54:27 -0400

    completed rake task seeding 120 days from s&p

--- server/bear_api/db/migrate/20170417203733_create_game_rounds.rb
+++ server/bear_api/db/migrate/20170417203733_create_game_rounds.rb
@@ -3,10 +3,8 @@ class CreateGameRounds < ActiveRecord::Migration[5.0]
     create_table :game_rounds do |t|
       t.string :underlying_asset_type
       t.timestamp[] :time_values
-      t.float8[] :price_over_time
+      t.float8[] :price_values
       t.string :name
-
-      t.timestamps
     end
   end
 end
