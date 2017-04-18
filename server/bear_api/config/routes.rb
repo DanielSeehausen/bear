c618cc63f7180df3a9b9d2d87834315129908913
github username <github email address>
2017-04-18 09:52:32 -0400

    Added more Auth info

--- server/bear_api/config/routes.rb
+++ server/bear_api/config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   resources :user_games
   resources :game_rounds
   resources :users
+  resources :sessions, only: [:create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
