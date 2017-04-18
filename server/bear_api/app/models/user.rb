b68531e5632879b9af482e6e66d88520756560b8
github username <github email address>
2017-04-18 09:21:36 -0400

    Authentication and Sessions Controller

--- server/bear_api/app/models/user.rb
+++ server/bear_api/app/models/user.rb
@@ -1,2 +1,29 @@
+require 'securerandom'
 class User < ApplicationRecord
+  before_create :set_auth_token
+
+  def self.authenticate(username, password)
+    user = User.find_by(username: username)
+    user && user.authenticate(password)
+  end
+
+  def self.from_token(token)
+    user_id = Auth.decode(token)["user_id"]
+    User.find(user_id)
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:username, :password)
+  end
+
+  def set_auth_token
+    return if auth_token.present?
+    self.auth_token = generate_auth_token
+  end
+
+  def generate_auth_token
+    SecureRandom.uuid.gsub(/\-/,'')
+  end
+
 end
