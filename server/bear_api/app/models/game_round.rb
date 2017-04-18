f1674e4059fb30a635a15f05db6238f460e96f1f
NickAnaya618 <anaya.nicholas@gmail.com>
2017-04-18 14:08:06 -0400

    created bear_api_development database. Migrated. Fixed table types. Incorporated Associations

--- server/bear_api/app/models/game_round.rb
+++ server/bear_api/app/models/game_round.rb
@@ -1,2 +1,4 @@
 class GameRound < ApplicationRecord
+  has_many :user_games
+  has_many :users, through: :user_games
 end
--- server/bear_api/db/migrate/20170417203733_create_game_rounds.rb
+++ server/bear_api/db/migrate/20170417203733_create_game_rounds.rb
@@ -2,8 +2,8 @@ class CreateGameRounds < ActiveRecord::Migration[5.0]
   def change
     create_table :game_rounds do |t|
       t.string :underlying_asset_type
-      t.timestamp[] :time_values
-      t.float8[] :price_values
+      t.timestamp :time_values, array: true
+      t.float :price_values, array: true
       t.string :name
     end
   end
