e67898c3a29be14b6e4a0c82bfc57edbe49d8803
NickAnaya618 <anaya.nicholas@gmail.com>
2017-04-21 12:04:29 -0400

    pushing updated state for about component

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -8,7 +8,6 @@ import Home from './components/home/home.component.jsx'
 import Game from './components/game/game.component.jsx'
 
 
-<<<<<<< HEAD
 (() => {
   let username = localStorage.getItem("username") || "Anon"
   let uniqueId = localStorage.getItem("uniqueId") || false
@@ -16,14 +15,7 @@ import Game from './components/game/game.component.jsx'
   dispatch.reconcileUserProfile(username, uniqueId, status)
   dispatch.getStockData("DEFAULT")
 })()
-=======
-// (function manageProfile() {
-//   let username = localStorage.getItem("username") || "anon"
-//   let uniqueId = localStorage.getItem("uniqueId") || false
-//   let status = uniqueId ? "Found" : "Newly Created"
-//   dispatch.setUserProfile(username, uniqueId, status)
-// })()
->>>>>>> 84f7318adc645df00b82ed55a9d20811f690dffb
+
 
 ReactDOM.render(
   <Router history={browserHistory}>
