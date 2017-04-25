d89b1935c50f97a283dbec556debf7b7afd474b8
github username <github email address>
2017-04-25 13:56:39 -0400

    heroku api

--- client/src/rails_api/dispatch.js
+++ client/src/rails_api/dispatch.js
@@ -1,6 +1,6 @@
 import { stockDataController } from '../controllers/stockDataController.js'
 
-const endpoint = 'http://localhost:3000'
+const endpoint = 'beargame-api.herokuapp.com'
 const defaultUserProfile = { msg_data: {
     status: "Newly Created",
     username: "Anon",
