4a42f6d0470d4455e2bd519f17cfdcfcdde6f8b2
github username <github email address>
2017-07-19 12:16:35 -0400

    No Updates

--- server/bear_api/app/controllers/game_rounds_controller.rb
+++ server/bear_api/app/controllers/game_rounds_controller.rb
@@ -2,30 +2,30 @@ class GameRoundsController < ApplicationController
 
   @@game_round_config = {starting_capital: 100000}
   @@default_stocks = [
-  ["Expedia", "EXPE"],
-  ["Intuitive Surgical", "ISRG"],
-  ["Whole Foods Market", "WFM"],
-  ["Amazon.com", "AMZN"],
-  ["Priceline", "PCLN"],
-  ["Akamai Technologies", "AKAM"],
-  ["First Solar", "FSLR"],
-  ["Netflix", "NFLX"],
-  ["S&P 500 IDX", "^GSPC"],
-  ["Herbalife", "HLF"],
-  ["Alphabet", "GOOG"],
-  ["BlackBerry", "BBRY"],
-  ["American International Group", "AIG"],
-  ["Vivus", "VVUS"],
-  ["Nicholas Financial", "NICK"],
-  ["Lazard", "LAZ"],
-  ["Goldman Sachs", "GS"],
-  ["BNP Paribas", "BNP.PA"],
-  ["The Houses of Lazard", "LAZ"],
-  ["BlackRock", "BLK"],
-  ["Activision Blizzard", "ATVI"],
-  ["Newmont Mining", "NEM"],
-  ["Trina Solar", "TSL"]
-]
+    ["Expedia", "EXPE"],
+    ["Intuitive Surgical", "ISRG"],
+    ["Whole Foods Market", "WFM"],
+    ["Amazon.com", "AMZN"],
+    ["Priceline", "PCLN"],
+    ["Akamai Technologies", "AKAM"],
+    ["First Solar", "FSLR"],
+    ["Netflix", "NFLX"],
+    ["S&P 500 IDX", "^GSPC"],
+    ["Herbalife", "HLF"],
+    ["Alphabet", "GOOG"],
+    ["BlackBerry", "BBRY"],
+    ["American International Group", "AIG"],
+    ["Vivus", "VVUS"],
+    ["Nicholas Financial", "NICK"],
+    ["Lazard", "LAZ"],
+    ["Goldman Sachs", "GS"],
+    ["BNP Paribas", "BNP.PA"],
+    ["The Houses of Lazard", "LAZ"],
+    ["BlackRock", "BLK"],
+    ["Activision Blizzard", "ATVI"],
+    ["Newmont Mining", "NEM"],
+    ["Trina Solar", "TSL"]
+  ]
 
   def pack_game_round_data(game_round)
     return {status: 'Found',
@@ -56,7 +56,7 @@ class GameRoundsController < ApplicationController
   def serve_game_data
     params[:ticker] == "DEFAULT" ? serve_default_game_data : serve_single_stock_data(params[:ticker])
   end
-  
+
   # protected
   #
   # def authenticate
